0fa6104f75e129026c7f4aa54e13e0f62aa5beb1
KBeginner <wyyking@126.com>
2018-07-16 21:04:54 +0800

    vue-pizza-app

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,31 +1,59 @@
 <template>
-  <el-form label-width="50px">
+  <el-form label-width="50px" @submit.native.prevent>
     <el-row class="login-title" type="flex" justify="center">
       <el-col :sapn="24"><img src="../assets/icon.png" /></el-col>
     </el-row>
     <el-form-item label="账号">
-      <el-input v-model="user.account" placeholder="请输入账号" required="required"></el-input>
+      <el-input type="email" v-model="user.account" placeholder="请输入账号" required="required"></el-input>
     </el-form-item>
     <el-form-item label="密码">
-      <el-input v-model="user.password" placeholder="请输入密码" required="required"></el-input>
+      <el-input type="password" v-model="user.password" placeholder="请输入密码" required="required"></el-input>
     </el-form-item>
     <el-form-item class="submit-btn">
-      <button>登录</button>
+      <button @click="login">登录</button>
     </el-form-item>
   </el-form>
 </template>
 
 <script>
+  import axios from 'axios'
     export default {
         name: "Login",
       data(){
           return {
             user:{
-              account: null,
-              password:null
-            }
+              account: '',
+              password:''
+            },
           }
-      }
+      },
+      methods:{
+        login(){
+          axios.get('/users.json')
+            .then(response=>{
+              let users = [];
+              for (let key in response.data){
+                users.push(response.data[key])
+              }
+              /*使用过滤器来匹配用户输入的账号和密码是否已经注册*/
+              let result = users.filter((user)=>{   //若匹配成功返回一个符合条件的数组，若匹配失败返回空数组
+                return this.user.account === user.account && this.user.password === user.password
+              });
+              if (result !=null && result.length >0){     //返回数组不为空且长度大于0，说明账号密码正确
+                this.$message({type:'info',message:'登录成功'});
+                setTimeout(()=>{
+                  this.$router.push('/')      //登录成功后跳转首页
+                },1000)
+              } else {    //否则提示错误
+                this.$alert('账号或密码错误',{
+                  type:'error',
+                  callback:action=>{}
+                })
+              }
+            });
+        }
+      },
+
     }
 </script>
 
--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -1,15 +1,100 @@
 <template>
   <el-container class="app-main">
-    menu
+    <el-row class="menu-main">
+      <el-col :span="15" class="about-content">
+        <el-container direction="vertical">
+          <el-row class="about-content-head">
+            <h4>披萨列表</h4>
+          </el-row>
+          <el-row class="menu-item">
+            <el-col :span="6">{{listNav.category}}</el-col>
+            <el-col :span="6">{{listNav.size}}</el-col>
+            <el-col :span="6">{{listNav.price}}</el-col>
+            <el-col :span="6">{{listNav.order}}</el-col>
+          </el-row>
+          <el-row class="menu-item" :key="obj.id" v-for="obj in pizzaList">
+            <el-col :span="6" class="item-pizza-name">{{obj.name}}</el-col>
+            <el-col :span="18" :key="item.row" v-for="item in obj.lists">
+              <el-row type="flex" justify="end">
+                <el-col :span="8">{{item.size}}</el-col>
+                <el-col :span="8">{{item.price}}</el-col>
+                <el-col :span="8"><el-button><i class="el-icon-plus"></i></el-button></el-col>
+              </el-row>
+            </el-col>
+          </el-row>
+        </el-container>
+      </el-col>
+      <el-col :span="8" :offset="1">shopping trolley</el-col>
+    </el-row>
   </el-container>
 </template>
 
 <script>
     export default {
-        name: "Menu"
+        name: "Menu",
+      data(){
+          return {
+            listNav:{
+              category:'分类',
+              size:'尺寸',
+              price:'价格',
+              order:'购买'
+            },
+            pizzaList:[
+              {
+                id:1,
+                name:'芝士披萨',
+                lists:[
+                  {
+                    row:1,
+                    size:'9寸',
+                    price:'38￥',
+                  },
+                  {
+                    row:2,
+                    size:'12寸',
+                    price:'48￥',
+                  }
+                ]
+              },
+              {
+                id:2,
+                name:'榴莲披萨',
+                lists:[
+                  {
+                    row:1,
+                    size:'9寸',
+                    price:'38￥',
+                  },
+                  {
+                    row:2,
+                    size:'12寸',
+                    price:'48￥',
+                  }
+                ]
+              }
+            ]
+          }
+      },
+      created(){
+
+      }
     }
 </script>
 
-<style scoped>
+<style scoped lang="less">
+  .menu-main{
+    width: 100%;
+    .about-content-head{
+      h4{
+        text-align: center;
+      }
+    }
+  .menu-item{
+    text-align: center;
+    .item-pizza-name{
 
+    }
+  }
+  }
 </style>
